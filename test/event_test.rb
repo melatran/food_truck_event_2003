3a2a2388cfd6063bbd06acfc701cdf7d8f010b31
Melanie Tran <melaniexx02@gmail.com>
2020-04-15 10:34:33 -0600

    test for food trucks that sell

--- test/event_test.rb
+++ test/event_test.rb
@@ -43,4 +43,11 @@ class EventTest < Minitest::Test
     @event.add_food_truck(@food_truck3)
     assert_equal ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @event.food_truck_names
   end
+
+  def test_can_get_food_trucks_that_sell_item
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    assert_equal [@food_truck1, @food_truck3], @event.food_trucks_that_sell(@item1)
+  end
 end
