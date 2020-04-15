f9fb7cb6236ff1db81577b1f60f52cd4bd41d1e0
Melanie Tran <melaniexx02@gmail.com>
2020-04-15 10:42:09 -0600

    Add potential revenue test

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -36,4 +36,19 @@ class FoodTruckTest < Minitest::Test
     expected = {@item1 => 55, @item2 => 12}
     assert_equal expected, @food_truck.inventory
   end
+
+  def test_can_get_revenue
+    @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)
+
+    assert_equal 148.75, @food_truck1.potential_revenue
+    assert_equal 345.00, @food_truck2.potential_revenue
+    assert_equal 243,75, @food_truck3.potential_revenue
+  end
 end
