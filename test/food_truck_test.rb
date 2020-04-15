8f136fbb18c6cb4ed8424e09548bc1977c58fb00
Melanie Tran <melaniexx02@gmail.com>
2020-04-15 10:14:31 -0600

    Add method for testing adding stock to inventory

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -25,6 +25,7 @@ class FoodTruckTest < Minitest::Test
 
   def test_add_stock_to_inventory
     @food_truck.stock(@item1, 30)
+    expected = {@item1 => 30}
     assert_equal expected, @food_truck.inventory
   end
 end
