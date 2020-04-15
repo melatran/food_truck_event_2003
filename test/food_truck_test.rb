15d21485507a05b02e0d56ed16f7b5e353b1a2cf
Melanie Tran <melaniexx02@gmail.com>
2020-04-15 10:50:48 -0600

    Works in pry but wrong answer in test

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -7,6 +7,8 @@ class FoodTruckTest < Minitest::Test
   def setup
     @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
   end
 
