d9464fd352dc04b14383d059bed1d4c5446b3c12
Melanie Tran <melaniexx02@gmail.com>
2020-04-15 10:16:39 -0600

    Updated test to match iter 1

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -27,5 +27,13 @@ class FoodTruckTest < Minitest::Test
     @food_truck.stock(@item1, 30)
     expected = {@item1 => 30}
     assert_equal expected, @food_truck.inventory
+    assert_equal 30, @food_truck.check_stock(@item1)
+
+    @food_truck.stock(@item1, 25)
+    assert_equal 55, @food_truck.check_stock(@item1)
+
+    @food_truck.stock(@item2, 12)
+    expected = {@item1 => 55, @item2 => 12}
+    assert_equal expected, @food_truck.inventory
   end
 end
