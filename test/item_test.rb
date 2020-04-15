1bcc24a8e88a9a8d19ee0a43c179be1ad94924ca
Melanie Tran <melaniexx02@gmail.com>
2020-04-15 10:05:03 -0600

    Test for attributes

--- test/item_test.rb
+++ test/item_test.rb
@@ -11,4 +11,9 @@ class ItemTest < Minitest::Test
   def test_it_exists
     assert_instance_of Item, @item1
   end
+
+  def test_it_has_attributes
+    assert_equal "Apple Pie (Slice)", @item2.name
+    assert_equal 2.50, @item2.price
+  end
 end
