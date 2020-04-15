b1c3633b51725c92aab771fb697cd8b9ffb81434
Melanie Tran <melaniexx02@gmail.com>
2020-04-15 11:34:54 -0600

    Fixed total_inventory and passed. Check names item1 not item_1

--- lib/event.rb
+++ lib/event.rb
@@ -21,4 +21,33 @@ class Event
     # require "pry";binding.pry
     end
   end
+  # 
+  # def list_items
+  #   inventories = @food_trucks.map {|food_truck| food_truck.inventory}
+  #   item_names = inventories.flat_map {|inventory| inventory.keys}
+  #   item_names.uniq
+  # end
+
+  def total_inventory
+    total = {}
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, quantity|
+        total[item] = {quantity: 0, food_trucks:[]} if total[item].nil?
+        total[item][:quantity] += quantity
+        total[item][:food_trucks] << food_truck
+      end
+    end
+    total
+  end
 end
+
+# class Hash
+#   def compact(opts={})
+#     inject({}) do |new_hash, (k,v)|
+#       if !v.nil?
+#         new_hash[k] = opts[:recurse] && v.class == Hash ? v.compact(opts) : v
+#       end
+#       new_hash
+#     end
+#   end
+# end
--- test/event_test.rb
+++ test/event_test.rb
@@ -19,7 +19,6 @@ class EventTest < Minitest::Test
     @food_truck2.stock(@item4, 50)
     @food_truck2.stock(@item3, 25)
     @food_truck3.stock(@item1, 65)
-    @food_truck3.stock(@item3, 10)
   end
 
   def test_it_exists
@@ -52,16 +51,50 @@ class EventTest < Minitest::Test
     assert_equal [@food_truck1, @food_truck3], @event.food_trucks_that_sell(@item1)
   end
 
+  # def test_can_list_items
+  #   @event.add_food_truck(@food_truck1)
+  #   @event.add_food_truck(@food_truck2)
+  #   @event.add_food_truck(@food_truck3)
+  #   assert_equal [@item1, @item3, @item3, @item4], @event.list_items
+  # end
+
   def test_can_get_total_inventory
-    @event.add_food_truck(@food_truck1)
-    @event.add_food_truck(@food_truck2)
-    @event.add_food_truck(@food_truck3)
+    event = Event.new("South Pearl Street Farmers Market")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
     expected = {
-      @item_1 => {quantity: 100, food_trucks: [@food_truck1, @food_truck3]},
-      @item_2 => {quantity: 7, food_trucks: [@food_truck1,]},
-      @item_3 => {quantity: 35, food_trucks: [@food_truck2, @food_truck3]},
-      @item_4 => {quantity: 50, food_trucks: [@food_truck2]}
+      item1 => {
+        quantity: 100,
+        food_trucks: [food_truck1, food_truck3]
+      },
+      item2 => {
+        quantity: 7,
+        food_trucks: [food_truck1,]
+      },
+      item3 => {
+        quantity: 35,
+        food_trucks: [food_truck2, food_truck3]
+      },
+      item4 => {
+        quantity: 50,
+        food_trucks: [food_truck2]
+      }
     }
-    assert_equal expected, @event.total_inventory
+    assert_equal expected, event.total_inventory
   end
 end
