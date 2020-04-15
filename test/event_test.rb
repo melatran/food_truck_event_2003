d09200ed7b911b62058777369783131090a3e351
Melanie Tran <melaniexx02@gmail.com>
2020-04-15 10:30:15 -0600

    Test for names of foodtrucks

--- test/event_test.rb
+++ test/event_test.rb
@@ -36,4 +36,8 @@ class EventTest < Minitest::Test
     @event.add_food_truck(@food_truck3)
     assert_equal [@food_truck1, @food_truck2, @food_truck3], @event.food_trucks
   end
+
+  def test_can_get_names_of_food_trucks
+    assert_equal ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @event.food_truck_names
+  end
 end
